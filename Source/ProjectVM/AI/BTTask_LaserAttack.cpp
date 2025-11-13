27e250d0fc058656f8b384f043a6b864d936a979
soloplay <somnium1108@gmail.com>
2025-11-13 21:03:13 +0900

    Hotfix: 잘못된 Laser인터페이스를 지우고 테스트를 안하고 넣음 -> 빌드 에러

--- Source/ProjectVM/AI/BTTask_LaserAttack.cpp
+++ Source/ProjectVM/AI/BTTask_LaserAttack.cpp
@@ -6,7 +6,6 @@
 #include "AIController.h"
 #include "AI/VMEnemyBase.h"
 #include "BehaviorTree/BlackboardComponent.h"
-#include "Interface/LaserAttackInterface.h"
 
 EBTNodeResult::Type UBTTask_LaserAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {

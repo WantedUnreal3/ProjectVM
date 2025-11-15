0daca2ddff03da2b2b1530905c16720d8d7c9baf
soloplay <somnium1108@gmail.com>
2025-11-15 17:48:09 +0900

    Bug: Need Test

--- Source/ProjectVM/AI/BTDecorator_AttackRange.cpp
+++ Source/ProjectVM/AI/BTDecorator_AttackRange.cpp
@@ -27,6 +27,7 @@ bool UBTDecorator_AttackRange::CalculateRawConditionValue(UBehaviorTreeComponent
 	// AI 컨트롤러 확인
 	if (AIControllerPtr == nullptr)
 	{
+		UE_LOG(LogTemp, Log, TEXT("내가 null인가1"));
 		return false;
 	}
 
@@ -35,6 +36,7 @@ bool UBTDecorator_AttackRange::CalculateRawConditionValue(UBehaviorTreeComponent
 	// 제어하는 폰이 없는 경우 함수 종료.
 	if (PawnPtr == nullptr)
 	{
+		UE_LOG(LogTemp, Log, TEXT("내가 null인가2"));
 		return false;
 	}
 
@@ -43,6 +45,7 @@ bool UBTDecorator_AttackRange::CalculateRawConditionValue(UBehaviorTreeComponent
 	// 형변환에 실패하면 함수 종료.
 	if (AIPawnPtr == nullptr)
 	{
+		UE_LOG(LogTemp, Log, TEXT("내가 null인가3"));
 		return false;
 	}
 
@@ -50,14 +53,16 @@ bool UBTDecorator_AttackRange::CalculateRawConditionValue(UBehaviorTreeComponent
 	UBlackboardComponent* BBComponentPtr = OwnerComp.GetBlackboardComponent();
 	if (BBComponentPtr == nullptr)
 	{
+		UE_LOG(LogTemp, Log, TEXT("내가 null인가4"));
 		return false;
 	}
 
 	// 블랙보드에 저장된 캐릭터 정보 가져오기.
-	APawn* Target = Cast<APawn>(BBComponentPtr->GetValueAsObject(BBKEY_TARGET));
+	APawn* Target = Cast<APawn>(BBComponentPtr->GetValueAsObject(TEXT("EnemyTarget")));
 	// Target이 null이면 함수 종료.
 	if (Target == nullptr)
 	{
+		UE_LOG(LogTemp, Log, TEXT("내가 null인가5"));
 		return false;
 	}
 
@@ -65,10 +70,12 @@ bool UBTDecorator_AttackRange::CalculateRawConditionValue(UBehaviorTreeComponent
 	float DistanceToTarget = PawnPtr->GetDistanceTo(Target);
 
 	// 공격 범위 가져오기. // Todo: 하드코딩 지우기
-	float AttackRangetWithRadius = 100;
+	float AttackRangetWithRadius = AIPawnPtr->GetAIAttackRange();
 
 	// 캐릭터와의 거리와 공격 가능 거리를 비교 후 Result에 저장.
 	bResult = (DistanceToTarget <= AttackRangetWithRadius);
+	bResult = BBComponentPtr->GetValueAsBool(TEXT("bCanAttack"));
+	UE_LOG(LogTemp, Display, TEXT("DistanceToTarget:%f AttackRangetWithRadius:%f, flag:%d"), DistanceToTarget, AttackRangetWithRadius, bResult);
 
 	return bResult;
 }

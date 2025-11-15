50fe4b55abfa5045575374cc2136472114077018
soloplay <somnium1108@gmail.com>
2025-11-15 17:36:29 +0900

    Feat: 보스 소환 근접 미니언 공격 체크 AnimNotify 구현
    - 공격 시 충돌 체크 용도

---
+++ Source/ProjectVM/Animation/AnimNotify_EnemySpawnNormalCheck.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/AnimNotify_EnemySpawnNormalCheck.h"
+#include "AnimNotify_EnemySpawnNormalCheck.h"
+
+#include "Interface/VMAIEnemyBaseInterface.h"
+
+UAnimNotify_EnemySpawnNormalCheck::UAnimNotify_EnemySpawnNormalCheck()
+{
+
+}
+
+void UAnimNotify_EnemySpawnNormalCheck::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	UE_LOG(LogTemp, Log, TEXT("여기 제발 와주라1"));
+
+	IVMAIEnemyBaseInterface* IEnemyBasePtr = Cast<IVMAIEnemyBaseInterface>(MeshComp->GetOwner());
+
+	if (IEnemyBasePtr == nullptr)
+	{
+		return;
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("여기 제발 와주라2"));
+	// 노말 공격 범위만큼 체크
+	IEnemyBasePtr->NormalAttackCheck();
+}
---
+++ Source/ProjectVM/Animation/AnimNotify_EnemySpawnNormalCheck.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_EnemySpawnNormalCheck.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTVM_API UAnimNotify_EnemySpawnNormalCheck : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	UAnimNotify_EnemySpawnNormalCheck();
+
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};

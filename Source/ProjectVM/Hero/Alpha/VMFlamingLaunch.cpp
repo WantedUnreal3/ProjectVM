f10522a6802be3c2fff29738f17b99d86772e737
oeccsy <oeccsy@gmail.com>
2025-11-17 12:53:09 +0900

    feat : Hero Alpha 이동스킬 추가

--- Source/ProjectVM/Hero/Alpha/VMFlamingLaunch.cpp
+++ Source/ProjectVM/Hero/Alpha/VMFlamingLaunch.cpp
@@ -3,12 +3,32 @@
 
 #include "Hero/Alpha/VMFlamingLaunch.h"
 
+#include "GameFramework/PawnMovementComponent.h"
+#include "Hero/VMCharacterHeroBase.h"
+#include "Hero/VMHeroStatComponent.h"
+#include "Hero/HeroStat.h"
+
 UVMFlamingLaunch::UVMFlamingLaunch(const FObjectInitializer& ObjectInitializer)
 {
+	SkillName = TEXT("FlamingLaunch");
+	SkillDesc = TEXT("캐릭터가 바라보는 방향으로 빠르게 대쉬합니다. 이동속도가 빠를수록 더 멀리 대쉬합니다.");
+	ManaCost = 10;
+	Cooldown = 2.0f;
+	RemainingCooldown = 0.0f;
 }
 
 void UVMFlamingLaunch::ActivateSkill(class AVMCharacterHeroBase* InOwner, class UVMHeroStatComponent* StatComp)
 {
 	Super::ActivateSkill(InOwner, StatComp);
-	UE_LOG(LogTemp, Log, TEXT("스킬 : FlamingLaunch"));
+	
+	if (InOwner->GetMovementComponent()->IsFalling())
+	{
+		FVector Direction = InOwner->GetActorForwardVector();
+		InOwner->LaunchCharacter(Direction * (500 + StatComp->GetStat().Speed), true, false);
+	}
+	else
+	{
+		FVector Direction = (InOwner->GetActorForwardVector() + InOwner->GetActorUpVector()).GetSafeNormal();
+		InOwner->LaunchCharacter(Direction * (500 + StatComp->GetStat().Speed), true, true);
+	}
 }

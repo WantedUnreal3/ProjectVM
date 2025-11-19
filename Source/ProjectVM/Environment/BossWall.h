c3190a0cfe0427872b6db5a62b7418db16b9a762
soloplay <somnium1108@gmail.com>
2025-11-20 01:39:57 +0900

    Add: Phase2 전용 C++ 파일 추가
    - 블루 프린트에서 함수 구현하도록 설계

---
+++ Source/ProjectVM/Environment/BossWall.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BossWall.generated.h"
+
+UCLASS()
+class PROJECTVM_API ABossWall : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABossWall();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};

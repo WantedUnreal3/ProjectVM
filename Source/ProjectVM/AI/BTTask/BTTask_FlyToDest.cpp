02cd2cc791578b5ae31765c3d10b84e47c2f1f22
soloplay <somnium1108@gmail.com>
2025-11-20 18:35:57 +0900

    Feat: Phase2에서 사용할 무브(버그 조금 있음)
    - 목적 위치 구하고 움직임.

---
+++ Source/ProjectVM/AI/BTTask/BTTask_FlyToDest.cpp
@@ -0,0 +1,69 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/BTTask/BTTask_FlyToDest.h"
+
+#include "AIController.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_FlyToDest::UBTTask_FlyToDest()
+{
+    NodeName = "Fly To Destination";
+    bNotifyTick = true; // TickTask 사용하려면 true로 설정
+}
+
+EBTNodeResult::Type UBTTask_FlyToDest::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    // Tick에서 이동 처리할 거라 바로 InProgress 반환
+    return EBTNodeResult::InProgress;
+}
+
+void UBTTask_FlyToDest::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+    AAIController* AICon = OwnerComp.GetAIOwner();
+    if (AICon == nullptr)
+    {
+        return;
+    }
+
+    APawn* Pawn = AICon->GetPawn();
+    if (Pawn == nullptr)
+    {
+        return;
+    }
+
+    UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+    if (BlackboardComp == nullptr)
+    {
+        return;
+    }
+
+    FVector Dest = BlackboardComp->GetValueAsVector(DestPositionKey.SelectedKeyName);
+    FVector Current = Pawn->GetActorLocation();
+
+    // 이동용 방향 (Z 포함)
+    FVector MoveDir = Dest - Current;
+    float Distance = MoveDir.Size();
+
+    if (Distance <= AcceptanceRadius)
+    {
+        FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+        return;
+    }
+
+    // 회전용 방향 (Z 제외)
+    FVector LookDir = MoveDir;
+    LookDir.Z = 0;
+    if (!LookDir.IsNearlyZero())
+    {
+        FRotator TargetRot = LookDir.Rotation();
+        Pawn->SetActorRotation(FRotator(0.f, TargetRot.Yaw, 0.f));
+    }
+
+    // 이동
+    FVector MoveDelta = MoveDir.GetSafeNormal() * MoveSpeed * DeltaSeconds;
+    Pawn->AddActorWorldOffset(MoveDelta, true);
+
+    // 몽타주 재생 (원하면 추가)
+}
---
+++ Source/ProjectVM/AI/BTTask/BTTask_FlyToDest.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_FlyToDest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTVM_API UBTTask_FlyToDest : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_FlyToDest();
+
+public:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector DestPositionKey;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float MoveSpeed = 300.0f; // 단위: cm/s
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float AcceptanceRadius = 50.0f; // 목표 도착 판정 반경
+	
+};

0e6bce4a3a7198bc4434c034f98a70d89ced7534
soloplay <somnium1108@gmail.com>
2025-11-20 20:40:55 +0900

    Feat: Phase의 Percent 시스템 구현
    - Phase 2는 각각의 퍼센트에 따라 구현. Task에서 결정되고, Decorator로 확인

---
+++ Source/ProjectVM/AI/BTDecorator/BTDecorator_CheckPercent.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/BTDecorator/BTDecorator_CheckPercent.h"
+
+#include "AIController.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTDecorator_CheckPercent::UBTDecorator_CheckPercent()
+{
+}
+
+bool UBTDecorator_CheckPercent::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	Super::CalculateRawConditionValue(OwnerComp, NodeMemory);
+
+	UBlackboardComponent* BBComp = OwnerComp.GetBlackboardComponent();
+	if (BBComp == nullptr)
+	{
+		return false;
+	}
+
+	// BlackBoard의 값 가져오기 (Int 형태로 저장되어 있다고 가정)
+	int32 CurrentPercentValue = BBComp->GetValueAsInt(TEXT("RandPercent"));
+
+	// 비교
+	return CurrentPercentValue <= PercentToCheck;
+}
+
+
---
+++ Source/ProjectVM/AI/BTTask/BTTask_DecidePercent.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/BTTask/BTTask_DecidePercent.h"
+
+#include "AIController.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_DecidePercent::UBTTask_DecidePercent()
+{
+}
+
+EBTNodeResult::Type UBTTask_DecidePercent::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    Super::ExecuteTask(OwnerComp, NodeMemory);
+
+    // AI ControllerPtr
+    AAIController* AIControllerPtr = Cast<AAIController>(OwnerComp.GetAIOwner());
+    if (AIControllerPtr == nullptr)
+    {
+        return EBTNodeResult::Failed;
+    }
+
+    int32 CurrentIndex = FMath::RandRange(MinimumPercent, MaximumPercent - 1);
+    UBlackboardComponent* BBComp = OwnerComp.GetBlackboardComponent();
+    if (BBComp == nullptr)
+    {
+        return EBTNodeResult::Failed;
+    }
+    BBComp->SetValueAsInt(TEXT("RandPercent"), CurrentIndex);
+
+    return EBTNodeResult::Succeeded;
+}
